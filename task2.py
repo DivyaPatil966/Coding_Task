2426b2ce22fe9208606117a3960bb9dc75d98d6a
divya <divyapatil4961@gmail.com>
2025-04-28 11:26:09 +0530

    change done

--- task2.py
+++ task2.py
@@ -1,31 +1,46 @@
-for i in range(5,11):
-    print(i,end=" ")
-print("\n")
-for i in range(11,5,-1):
-    print(i,end=" ")
-print("\n")
-string1="python"
-string2=" python programming"
-print("concatenation=",string1+string2)
-print("repitation=",string1*2)
-print("repetation=",string2*3)
-
-
-list=[]
-print("Blank List=",list)
-list=["python","programming"]
-print(list)
-print(list[1])
-print(list[0])
-
-dict=dict()
-print("empty dictionary")
-dict1={
-    "name":"Divya",
-    "Roll_no":326,
-    "Marks":45
-}
-print("student details=",dict1)
-print("1st dict:",dict1["name"])
-print("2nd dict:",dict1["Roll_no"])
-print("3rd dict:",dict1["Marks"])
+students=[]
+def add_student():
+    name=input("Enter name:\n")
+    roll_no=input("Enter roll_no:")
+    marks=input("Enter Marks:")
+    students.append({"name":name,"roll_no":roll_no,"marks":marks})
+    print("student added")
+def display_all_students():
+    if not students:
+        print("No students to display")
+        return
+    print("\n All students")
+    for student in students:
+        print("Name:",student['name'],"roll_no:",student['roll_no'],"marks:",student['marks'])
+              
+def search_by_roll_no(roll_no):
+    for student in students:
+        if student ["roll_no"]==roll_no:
+            print("student found")
+            print("name:",student["name"],"Roll_no:",student["roll_no"],"marks:",student["marks"],"\n")
+            return
+        print("student not found\n")
+            
+def main():
+    while True:
+        print("Enter your choice:\n")
+        print("1. add_students()\n")
+        print("2. display all students:\n")
+        print("3. Search by roll no:\n")
+        print("4. Exit\n")
+        choice=input("Choice:")
+        if choice =='1':
+            add_student()
+        elif choice=='2':
+            display_all_students()
+        elif choice=='3':
+            roll_no=input("enter roll_no to search:")
+            search_by_roll_no(roll_no)
+        elif choice=='4':
+            print("Exit")
+            break
+        else:
+            print("Invalid Choice\n")
+            
+if __name__=="__main__":
+    main()          
