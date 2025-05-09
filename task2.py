040629947f53b01ae5449ba4415b859e0e3a3ff7
divya <divyapatil4961@gmail.com>
2025-05-09 12:42:52 +0530

    Implemented exception handling for file operations and user input

--- task2.py
+++ task2.py
@@ -1,27 +1,49 @@
 import json
 
 def read_data():
-    with open ("students.json") as file:
-        students=json.load(file)
-        return students
-        
+    try:
+        with open ("students.json") as file:
+            students=json.load(file)
+            return students
+    except FileNotFoundError:
+        return[]
+          
 def write_data(students):
     with open("students.json","w") as file:
         json.dump(students,file,indent=4)
-       
         
+  
+
 def add_student():
-    students_details=read_data()
+    students = read_data()    
+    roll_no = [student["roll_no"] for student in students]
+
+    roll_number=input("Enter roll_no:")
+   
+          
+    if roll_number in roll_no:
+        print("Student with this roll number already exists.")
+        return
     name=input("Enter name:\n")
-    roll_no=input("Enter roll_no:")
-    physics=int(input("Enter Physics marks:"))
-    chemistry=int(input("Enter chemistry marks:"))
-    maths=int(input("Enter maths marks"))
-    marks={"physics":physics,"chemistry":chemistry,"maths":maths}
-    students_details.append({"name":name,"roll_no":roll_no,"marks":marks})
-    print("student added")
-    write_data(students_details)
+    try:
+        physics=int(input("Enter Physics marks:"))
+        chemistry=int(input("Enter chemistry marks:"))
+        maths=int(input("Enter maths marks:"))
+    except ValueError:
+        print("Invalid input. Marks should be integers")
+        return
+    students.append({
+        'name':name,
+        'roll_no':roll_number,
+        'marks':{
+            'physics':physics,
+            'chemistry':chemistry,
+            'maths':maths
+        }
+    })
     
+    write_data(students)
+    print("students added successfully")
     
 def display_all_students():
     students = read_data()
@@ -53,21 +75,20 @@ def search_by_roll_no(roll_no):
             
 def main():
     while True:
-        print("Enter your choice:\n")
+        print("Menu:\n")
         print("1. add_students()\n")
         print("2. display all students:\n")
         print("3. Search by roll no:\n")
         print("4. Exit\n")
-        choice=input("Choice:")
+        choice=input("Enter your Choice:")
         if choice =='1':
             add_student()
-            
-        elif choice=='2':
+        elif choice =='2':
             display_all_students()
-        elif choice=='3':
+        elif choice =='3':
             roll_no=input("enter roll_no to search:")
             search_by_roll_no(roll_no)
-        elif choice=='4':
+        elif choice =='4':
             print("Exit")
             break
         else:
