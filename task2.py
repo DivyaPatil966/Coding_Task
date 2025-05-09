184b106c7256d34105d31d6b2528487ffe8d340c
divya <divyapatil4961@gmail.com>
2025-05-09 10:09:39 +0530

    Add read_data and write_data functions for handling student records

--- task2.py
+++ task2.py
@@ -1,15 +1,30 @@
-students=[]
+import json
+
+def read_data():
+    with open ("students.json") as file:
+        students=json.load(file)
+        return students
+        
+def write_data(students):
+    with open("students.json","w") as file:
+        json.dump(students,file,indent=4)
+       
+        
 def add_student():
+    students_details=read_data()
     name=input("Enter name:\n")
     roll_no=input("Enter roll_no:")
     physics=int(input("Enter Physics marks:"))
     chemistry=int(input("Enter chemistry marks:"))
     maths=int(input("Enter maths marks"))
     marks={"physics":physics,"chemistry":chemistry,"maths":maths}
-    students.append({"name":name,"roll_no":roll_no,"marks":marks})
+    students_details.append({"name":name,"roll_no":roll_no,"marks":marks})
     print("student added")
+    write_data(students_details)
+    
     
 def display_all_students():
+    students = read_data()
     if not students:
         print("No students to display")
         return
@@ -23,6 +38,7 @@ def display_all_students():
         print("Maths:",student['marks']['maths'])
               
 def search_by_roll_no(roll_no):
+    students=read_data()
     for student in students:
         if student ["roll_no"]==roll_no:
             print("student found")
@@ -33,7 +49,7 @@ def search_by_roll_no(roll_no):
             print("Chemistry:",student['marks']['chemistry'])
             print("Maths:",student['marks']['maths'])
             return
-        print("student not found\n")
+    print("student not found\n")
             
 def main():
     while True:
@@ -45,6 +61,7 @@ def main():
         choice=input("Choice:")
         if choice =='1':
             add_student()
+            
         elif choice=='2':
             display_all_students()
         elif choice=='3':
